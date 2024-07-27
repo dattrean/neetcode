50da84f68fdbdcef8f61988a099cc7f7bd40635e
dattrean <168373542+dattrean@users.noreply.github.com>
2024-07-27 19:01:59 +0000

    Sat, Jul 27, 2024, 7:01 PM +00:00

--- src/minimum-cost-to-convert-string-i/solution.py
+++ src/minimum-cost-to-convert-string-i/solution.py
@@ -18,7 +18,31 @@ class Solution:
                 (ord(changed[conversion_index]) - ord("a"), cost[conversion_index])
             )
 
-        min_conversion_costs = [self.dijkstra(start_char, adjacency_list) for start_char in range(26)]
+        def dijkstra(
+            start: int, adjacency_list: list[list[tuple[int, int]]]
+        ) -> list[float]:
+            priority_queue = [(0, start)]
+            min_costs = [float("inf")] * 26
+
+            while priority_queue:
+                current_cost, current_char = heapq.heappop(priority_queue)
+
+                if min_costs[current_char] != float("inf"):
+                    continue
+
+                min_costs[current_char] = current_cost
+
+                for target_char, conversion_cost in adjacency_list[current_char]:
+                    new_total_cost = current_cost + conversion_cost
+
+                    if min_costs[target_char] == float("inf"):
+                        heapq.heappush(priority_queue, (new_total_cost, target_char))
+
+            return min_costs
+
+        min_conversion_costs = [
+            dijkstra(start_char, adjacency_list) for start_char in range(26)
+        ]
 
         total_cost = 0
         for s, t in zip(source, target):
@@ -31,25 +55,3 @@ class Solution:
                 total_cost += int(char_conversion_cost)
 
         return total_cost
-
-    def dijkstra(
-        self, start: int, adjacency_list: list[list[tuple[int, int]]]
-    ) -> list[float]:
-        priority_queue = [(0, start)]
-        min_costs = [float("inf")] * 26
-
-        while priority_queue:
-            current_cost, current_char = heapq.heappop(priority_queue)
-
-            if min_costs[current_char] != float("inf"):
-                continue
-
-            min_costs[current_char] = current_cost
-
-            for target_char, conversion_cost in adjacency_list[current_char]:
-                new_total_cost = current_cost + conversion_cost
-
-                if min_costs[target_char] == float("inf"):
-                    heapq.heappush(priority_queue, (new_total_cost, target_char))
-
-        return min_costs
