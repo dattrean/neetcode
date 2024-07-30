a8bbe098d7aaa5aabbf4b5b5c5de3e2f07dc402b
dattrean <168373542+dattrean@users.noreply.github.com>
2024-07-30 22:13:20 +0000

    Tue, Jul 30, 2024, 10:13 PM +00:00

--- src/minimum-deletions-to-make-string-balanced/solution.py
+++ src/minimum-deletions-to-make-string-balanced/solution.py
@@ -1,14 +1,17 @@
 s = "aababbab"
 
 stack = []
-
+count = 0
 
 for character in s:
-    if stack and character == "a":
+    if stack and stack[-1] == "b" and character == "a":
         stack.pop()
+        count += 1
     else:
         stack.append(character)
 
 
 for item in stack:
     print(item)
+
+print(count)
