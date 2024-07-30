ba98dedb9a0300bb6291f22934cc59a8a453db4e
dattrean <168373542+dattrean@users.noreply.github.com>
2024-07-30 22:21:51 +0000

    Tue, Jul 30, 2024, 10:21 PM +00:00

--- src/minimum-deletions-to-make-string-balanced/solution.py
+++ src/minimum-deletions-to-make-string-balanced/solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
+        # Create an empty stack to keep track of characters in the string
         stack: list[str] = []
+
+        # Create a counter to keep track of the number of deletions
         delete_count = 0
 
+        # Loop through all characters in `s`
         for character in s:
             if stack and character == "a" and stack[-1] == "b":
                 stack.pop()
